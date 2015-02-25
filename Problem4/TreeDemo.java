b0a1a34013d02c3a9fc639a532d9de196d9df4c7
Brandon Mikulka <brandon.mikulka@colorado.edu>
2015-02-25 15:04:59 -0800

    first refactor - create a factory of singletons

--- Problem4/TreeDemo.java
+++ Problem4/TreeDemo.java
@@ -31,15 +31,118 @@ class Tree implements Terrain
 	{
 		graphics.drawImage(image, x, y, null);
 	}
+    public static Tree getInstance(){
+        return null;
+    }
+}
+class AppleTree extends Tree
+{
+    private static Tree tree;
+
+    private AppleTree(){
+        super("Apple");
+    }
+
+    public static Tree getInstance(){
+        if (tree == null){
+            tree = new AppleTree();
+        }
+        return tree;
+    }
+}
+class LemonTree extends Tree
+{
+    private static Tree tree;
+
+    private LemonTree(){
+        super("Lemon");
+    }
+
+    public static Tree getInstance(){
+        if (tree == null){
+            tree = new LemonTree();
+        }
+        return tree;
+    }
+}
+class BlobTree extends Tree
+{
+    private static Tree tree;
+
+    private BlobTree(){
+        super("Blob");
+    }
+
+    public static Tree getInstance(){
+        if (tree == null){
+            tree = new BlobTree();
+        }
+        return tree;
+    }
+}
+class ElmTree extends Tree
+{
+    private static Tree tree;
+
+    private ElmTree(){
+        super("Elm");
+    }
+
+    public static Tree getInstance(){
+        if (tree == null){
+            tree = new ElmTree();
+        }
+        return tree;
+    }
+}
+class MapleTree extends Tree
+{
+    private static Tree tree;
+
+    private MapleTree(){
+        super("Maple");
+    }
+
+    public static Tree getInstance(){
+        if (tree == null){
+            tree = new MapleTree();
+        }
+        return tree;
+    }
 }
 class TreeFactory
 {
 	private static final ArrayList<Tree> mylist = new ArrayList<Tree>();
 	public static Terrain getTree(String type)
 	{
-		Tree tree = new Tree(type);
-		mylist.add(tree);
-		return tree;
+//		Original Code:
+//      Tree tree = new Tree(type);
+//		mylist.add(tree);
+//		return tree;
+
+        //Implement Factory Design Pattern
+        System.out.println("Enter Factory Class 'getTree()' method");
+        Tree tree = null;
+        if (type.equals("Apple")) {
+            tree = AppleTree.getInstance();
+        }
+        else if (type.equals("Lemon")) {
+            tree = LemonTree.getInstance();
+        }
+        else if (type.equals("Blob")) {
+            tree = BlobTree.getInstance();
+        }
+        else if (type.equals("Elm")) {
+            tree = ElmTree.getInstance();
+        }
+        else if (type.equals("Maple")) {
+            tree = MapleTree.getInstance();
+        }
+        // Ability to add more tree types here!
+        else {
+            System.out.println("Type is not an available tree type");
+        }
+        return tree;
    }
 }
 /**
@@ -49,7 +152,7 @@ class TreeDemo extends JPanel
 {
 	private static final int width = 800;
 	private static final int height = 700;
-	private static final int numTrees = 50;
+	private static final int numTrees = 5000;
 	private static final String type[] = { "Apple", "Lemon", "Blob", "Elm", "Maple" };
 
 	public void paint(Graphics graphics)
