1536be94e9fb62b59c033e85553639a688e6ddee
John-Luke Bucuvalas <johnluke@rgnt1-46-79-dhcp.int.colorado.edu>
2015-03-03 11:19:55 -0700

    added obeserver pattern for reporting test results

--- Problem2/Submission.java
+++ Problem2/Submission.java
@@ -5,10 +5,13 @@
 */
 
 import java.util.Random;
+import java.util.*;
 
 public class Submission
 {	
     private Random myRandom;
+    private List<iReporter> reports;
+   	private boolean lastTestPassed;
 	private boolean lastErrorWasTimeout;
 
     // You may add attributes to this class if necessary
@@ -17,22 +20,64 @@ public class Submission
 	{
 	    myRandom = new Random();
 		lastErrorWasTimeout = false;
+		reports = new ArrayList<iReporter>();
+		iReporter passedReport = new NumTestsPassedReport(this);
+		iReporter tOReport = new NumTestsTimeoutReport(this);
+		reports.add(passedReport);
+		reports.add(tOReport);
 	}
 
     public void runTestCase()
 	{
 	    // For now, randomly pass or fail, possibly due to a timeout
 		boolean passed = myRandom.nextBoolean();
+		lastTestPassed = passed;
 		if(!passed)
 		{
 		    lastErrorWasTimeout = myRandom.nextBoolean();
 		}
 		
-		// You can add to the end of this method for reporting purposes
+		notifyReports();
 	}
 	
     public boolean wasTimeoutError()
 	{
 	    return lastErrorWasTimeout;
+	}
+	public boolean didTestPass()
+	{
+		return lastTestPassed;
+	}
+
+	public void addReport(iReporter r){
+		reports.add(r);
+	}
+
+	public void notifyReports(){
+		for (iReporter i : reports){
+			i.notifyTestResult();
+		}
+	}
+
+	public void report(){
+		for (iReporter i : reports){
+			i.report();
+		}
+	}
+
+	public void runTests(){
+		runTests(20);
+	}
+
+	public void runTests(int numTests){
+		for (int i = 0; i < numTests; i++){
+			runTestCase();
+		}
+	}
+
+	public static void main(String[] args){
+		Submission test = new Submission();
+		test.runTests();
+		test.report();
 	}
 }
