a14cd62ad9293482802c214a648bd8f57c58b5ee
Brandon Mikulka <brandon.mikulka@colorado.edu>
2015-03-02 18:06:26 -0700

    Add singleton pattern to problem 1

--- Problem1/SubmissionQueue.java
+++ Problem1/SubmissionQueue.java
@@ -11,22 +11,36 @@ import java.util.concurrent.PriorityBlockingQueue;
 public class SubmissionQueue
 {
 	private PriorityBlockingQueue<Submission> internalQueue;
+    private static SubmissionQueue queue;
+
+    private SubmissionQueue(){
+        internalQueue = new PriorityBlockingQueue<Submission>();
+
+    }
+
+    public static SubmissionQueue getInstance(){
+        if (queue == null){
+            queue = new SubmissionQueue();
+        }
+        return queue;
+    }
+
 	public boolean add(Submission s){
 		return internalQueue.add(s);
 	}
-	public boolean process(){
-		Submission target;
-		try{target = internalQueue.take();}
-		catch(InterruptedException e){
-			//oh no
-			System.err.println(e);
-			return false;
-		};
-		//do some stuff
-		if(target == null) return false;
-		return true;
-	}
-	public SubmissionQueue(){
-		internalQueue = new PriorityBlockingQueue<Submission>();
-	}
+
+    public boolean process() {
+        Submission target;
+        try {
+            target = internalQueue.take();
+        } catch (InterruptedException e) {
+            //oh no
+            System.err.println(e);
+            return false;
+        }
+
+        //do some stuff to target
+
+        return (target != null);
+    }
 }
